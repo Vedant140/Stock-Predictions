1af41838a5e29e7143b84ce4ca6b2c5418d967e2
Vedant Padole <vedantpadole@Vedants-MacBook-Air.local>
2023-07-18 00:23:14 +0530

    Clear cache and rebuild

--- Stock.py
+++ Stock.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 import pandas_datareader as data
 import yfinance as yf
 from datetime import date
-from fbprophet import Prophet
+# from fbprophet import Prophet
 from plotly import graph_objs as go
 from keras.models import load_model
-from fbprophet.plot import plot_plotly
+# from fbprophet.plot import plot_plotly
 
 import streamlit as st
 
@@ -100,20 +100,20 @@ plt.ylabel('Price')
 plt.legend()
 st.pyplot(fig2)
 
-st.subheader('Prediction')
-n_years=st.slider("Years of prediction")
-period=n_years*365
-df_train=df[['Date','Close']]
-df_train=df_train.rename(columns={"Date":"ds","Close":"y"})
-m=Prophet()
-m.fit(df_train)
-future=m.make_future_dataframe(periods=period)
-forecast=m.predict(future)
-st.write(forecast.tail())
+# st.subheader('Prediction')
+# n_years=st.slider("Years of prediction")
+# period=n_years*365
+# df_train=df[['Date','Close']]
+# df_train=df_train.rename(columns={"Date":"ds","Close":"y"})
+# m=Prophet()
+# m.fit(df_train)
+# future=m.make_future_dataframe(periods=period)
+# forecast=m.predict(future)
+# st.write(forecast.tail())
 
-st.write('Final Forcast Data')
-fig1=plot_plotly(m,forecast)
-st.plotly_chart(fig1)
+# st.write('Final Forcast Data')
+# fig1=plot_plotly(m,forecast)
+# st.plotly_chart(fig1)
 
 # Expected Current value
 st.subheader('EXPECTED CURRENT VALUE')
